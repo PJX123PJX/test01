04d15a8ea753784b018c111937da82e89ad35bac
PengJingxi <pengjingxi@yjk.cn>
2024-06-27 09:44:33 +0800

    补充配置

--- 代码测试/src/设计模式/工厂方法模式.h
+++ 代码测试/src/设计模式/工厂方法模式.h
@@ -76,25 +76,25 @@ public:
 };
 
 
-//int main()
-//{
-//	TankFactory* factory56 = new Tank56Factory();
-//	Tank* tank56 = factory56->createTank();
-//	tank56->type();
-//
-//	TankFactory* factory96 = new Tank96Factory();
-//	Tank* tank96 = factory96->createTank();
-//	tank96->type();
-//
-//	delete tank96;
-//	tank96 = nullptr;
-//	delete factory96;
-//	factory96 = nullptr;
-//
-//	delete tank56;
-//	tank56 = nullptr;
-//	delete factory56;
-//	factory56 = nullptr;
-//
-//	return 0;
-//}
+int main1()
+{
+	TankFactory* factory56 = new Tank56Factory();
+	Tank* tank56 = factory56->createTank();
+	tank56->type();
+
+	TankFactory* factory96 = new Tank96Factory();
+	Tank* tank96 = factory96->createTank();
+	tank96->type();
+
+	delete tank96;
+	tank96 = nullptr;
+	delete factory96;
+	factory96 = nullptr;
+
+	delete tank56;
+	tank56 = nullptr;
+	delete factory56;
+	factory56 = nullptr;
+
+	return 0;
+}
